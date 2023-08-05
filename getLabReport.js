1e9f2c1c94f0ff84e5ec92608e1f0272aa37f2fc
basir5101 <basir.bsmrstu@gmail.com>
2023-08-06 01:19:30 +0600

    cover page done

---
+++ getLabReport.js
@@ -0,0 +1,143 @@
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+const getLabReport = async (req, res) => {
+    try {
+        const { assignment_topic, course_title, course_code, student_name, student_id, student_year, student_semester, student_session, student_department, teacher_name, teacher_position, teacher_department, teacher_university, submission_date } = req.body;
+        const imageUrl = `${process.env.DOMAIN}/images/logo/bsmrstu.jpg`;
+        const launchOptions = {
+            args: ['--no-sandbox'],
+            headless: "new"
+        };
+
+        const browser = await puppeteer.launch(launchOptions);
+        // const browser = await puppeteer.launch({ headless: "new" });
+        const page = await browser.newPage();
+
+        const htmlContent =
+            `<html>
+            <head>            
+                <style>
+                    body {
+                        margin: 70px;
+                        font-family: 'Times New Roman', Times, serif;
+                    }
+            
+                    table {
+                        border-collapse: collapse;
+                        width: 100%;
+                        table-layout: fixed;
+                        /* Add this line to fix the column widths */
+                    }
+            
+                    thead tr {
+                        background-color: #2d7136;
+                        color: white;
+                        font-size: 24px;
+                    }
+            
+                    td,
+                    th {
+                        border: 1px solid #dddddd;
+                        padding: 8px;
+                    }
+            
+                    tr:nth-child(even) {
+                        background-color: #dddddd;
+                    }
+            
+                    tr div {
+                        font-size: 24px;
+                        margin: 5px 10px;
+                    }
+                </style>
+
+            </head>
+
+            <body>
+                <h1 style="text-align: center; color: #2d7136;">BANGABANDHU SHEIKH MUJIBUR RAHMAN SCIENCE
+                    <br>
+                    &
+                    <br>
+                    TECHNOLOGY UNIVERSITY
+                </h1>
+                <br />
+                <div style='display: flex; justify-content: center; align-items: center'>
+                    <img style='height: 150px;' src=${imageUrl} alt="bsmrstu logo">
+                </div>
+                <h3 style="text-align: center; color: #2d7136;">GOPALGANJ-8100</h3>
+                <br />
+                <hr>
+                <br />
+                <h2 style="text-align: center; color: #2d7136;">Lab Report</h2>
+                <br />
+                <h2>Course Title: <span style="font-weight: lighter;">${course_title}</span></h2>
+                <h2>Course Code: <span style="font-weight: lighter;">${course_code}</span></h2>
+                <br />
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Submitted By</th>
+                            <th>Submitted To</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div><strong>Name: </strong> ${student_name} </div>
+                                <div><strong>Student Id: </strong>  ${student_id} </div>
+                                <div><strong>Year: </strong> ${student_year} </div>
+                                <div><strong>Semester: </strong> ${student_semester} </div>
+                                <div><strong>Session: </strong> ${student_session} </div>
+                                <div>Department of ${student_department}</div>
+                                <div>Bangabandhu Sheikh Mujibur Rahman
+                                    Science & Technology University,
+                                    <br>
+                                    Gopalganj-8100
+                                </div>
+                            </td>
+                            <td>
+                                <div><strong>Name: </strong> ${teacher_name} </div>
+                                <div> ${teacher_position} </div>
+                                <br>
+                                <br>
+                                <div>Department of ${teacher_department} </div>
+                                <div> ${teacher_university} </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br>
+                <div style="font-size: 20px;">Date of Submission: ${submission_date} </div>
+            </body>
+        </html>`
+        await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+
+        const buffer = await page.pdf({
+            format: 'A4',
+            margin: {
+                top: '20px',
+                right: '20px',
+                bottom: '20px',
+                left: '20px'
+            },
+            scale: 0.8, // Adjust this value as needed
+            printBackground: true,
+        });
+        await browser.close();
+
+        res.status(200).json({
+            status: 'success',
+            message: 'cover page created successfully',
+            data: { buffer, htmlContent }
+        })
+    } catch (error) {
+        res.status(200).json({
+            status: 'failed',
+            message: error?.message,
+            data: error
+        })
+    }
+};
+
+module.exports = { getLabReport };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { scrapeLogic } = require("./scrapeLogic");
+const { getLabReport } = require("./getLabReport");
 const app = express();
 
 app.use(cors());
@@ -8,9 +9,15 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
-app.post("/scrape", (req, res) => {
+app.post("/bsmrstu/assignment", (req, res) => {
   scrapeLogic(req, res);
 });
+
+
+app.post("/bsmrstu/lab", (req, res) => {
+  getLabReport(req, res);
+});
+
 
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
